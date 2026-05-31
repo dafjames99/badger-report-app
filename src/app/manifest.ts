ee0e8ca69e5ff94900eb35a75dc6596eae91e395
dafjames99 <d4fydd.thom4s@gmail.com>
2026-05-31 20:48:58 +0100

    Tweaking manifest icon handling.

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/badger-icon-new.png',
+        src: '/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
       },
